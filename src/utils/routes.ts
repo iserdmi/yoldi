b149884a9cc5efdf8f6c95cc94b1a54bde6467d8
Sergei Dmitriev <iserdmi@gmail.com>
2023-02-11 18:10:37 +0100

    Fix getUserRoutre

--- src/utils/routes.ts
+++ src/utils/routes.ts
@@ -2,7 +2,6 @@ export const getSignInRoute = () => '/auth/sign-in'
 export const getSignUpRoute = () => '/auth/sign-up'
 export const getSignOutRoute = () => '/auth/sign-out'
 export const getUsersRoute = () => '/'
-// export const getUserRoute = (slug: string) => `/xxx/xxx/${slug}`
-export const getUserRoute = (slug: string) => `/${slug}123`
+export const getUserRoute = (slug: string) => `/${slug}`
 export const getMyProfileRoute = () => `/my-profile`
 export const getEditProfileRoute = () => `/edit-profile`
